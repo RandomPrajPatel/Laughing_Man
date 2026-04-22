4c151b85ce5c0243c64ca2bcc035e837c40dcd56
Praj Patel <patelpraj0210@gmail.com>
2026-04-22 10:39:22 -0400

    Started Movement

--- laughing_man.js
+++ laughing_man.js
@@ -1,5 +1,6 @@
 const SQUARE_COUNT = 3;
-
+const TIMER_SPEED = 16.6;
+const SPEED  = 5; 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector("#square").addEventListener("click", () => {
         alert("OMG YOU CLICKED ME!");
@@ -11,6 +12,23 @@ document.addEventListener('DOMContentLoaded', () => {
         square.src = "laughing_man.jpg";
         square.alt = "Catch the Laughing Man!";
         square.className = "square";
-        box.appendChild(square); 
+        box.appendChild(square);
     }
+
+    Array.from(box.children).forEach((element) => {
+        let dx = SPEED * Math.random() * 2 - 1;
+        let dy = SPEED * Math.random() * 2 - 1;
+
+        let x = parseInt(element.style.left) || 225;
+        let y = parseInt(element.style.top) || 175;
+
+        setInterval(() => {
+            x += dx;
+            y += dy;
+
+            element.style.left = x + "px";
+            element.style.top = y + "px";
+
+        }, TIMER_SPEED)
+    });
 }); 
