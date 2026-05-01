8d1fe208cc542a9c5650c34c9cdede8cf6136edc
Praj Patel <patelpraj0210@gmail.com>
2026-05-01 01:54:05 -0400

    Change the border color when it hits the wall

--- laughing_man.js
+++ laughing_man.js
@@ -2,24 +2,28 @@ const SQUARE_COUNT = 30;
 const TIMER_SPEED = 16.6;
 const SPEED = 5;
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector("#square").addEventListener("click", () => {
+    
+    document.querySelector("#square").addEventListener("mouse", () => {
         alert("OMG YOU CLICKED ME!");
     });
     let box = document.querySelector("#box");
 
-    for (let i = 0; i < SQUARE_COUNT; i++) {
-        let square = document.createElement('img');
-        square.src = "laughing_man.jpg";
-        square.alt = "Catch the Laughing Man!";
-        square.className = "square";
-        box.appendChild(square);
-    }
+  for (let i = 0; i < SQUARE_COUNT; i++) {
+    let square = document.createElement('img');
+    square.src = "laughing_man.jpg";
+    square.alt = "Catch the Laughing Man!";
+    square.className = "square";
 
+    square.style.left = Math.random() * 450 + "px";
+    square.style.top = Math.random() * 350 + "px";
+
+
+    box.appendChild(square);
+}
     Array.from(box.children).forEach((element) => {
         const parent = element.parentElement;
         const maxX = parent.clientWidth - element.clientWidth;
         const maxY = parent.clientHeight - element.clientHeight;
-
 
         let dx = SPEED * (Math.random() * 2 - 1);
         let dy = SPEED * (Math.random() * 2 - 1);
@@ -30,11 +34,14 @@ document.addEventListener('DOMContentLoaded', () => {
         setInterval(() => {
 
             if (x <= 0 || x >= maxX) {
-                dx *= -1;
-            }
-            if (y < 0 || y >= maxY) {
-                dy *= -1;
-            }
+    dx *= -1;
+    element.style.borderColor = newColor();
+}
+
+if (y <= 0 || y >= maxY) {
+    dy *= -1;
+    element.style.borderColor = newColor();
+}
             x += dx;
             y += dy;
 
@@ -44,3 +51,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }, TIMER_SPEED)
     });
 }); 
+function newColor(){
+    let r = Math.floor (Math.random()*256); 
+    let g = Math.floor (Math.random()*256); 
+    let b = Math.floor (Math.random()*256); 
+    return `rgb(${r}, ${g}, ${b})`; 
+}
